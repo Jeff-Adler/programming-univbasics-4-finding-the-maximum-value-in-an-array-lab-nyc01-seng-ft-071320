a86e311b2eabb7819bac323672f72e19c381b784
Jeff Adler <jeffm.adler@gmail.com>
2020-06-09 21:27:29 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,8 @@
 def find_max_value(array)
-  # Add your solution here
+  max_value = array[0]
+  0...array.length |index| do 
+    if index > max_value 
+      max_value = index
+    end
+  end
 end
